da4ee12932f2da20b4cac37ed3507ad8088245ea
httpp4t0 <httpp4t0@DESKTOP-POM2PEE>
2023-09-05 21:55:10 -0300

    Estudio Prueba

---
+++ src/main/java/com/mycompany/estudioprueba/EstudioPrueba.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estudioprueba;
+
+/**
+ *
+ * @author httpp4t0
+ */
+public class EstudioPrueba {
+}
